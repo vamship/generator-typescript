59b3428c765f036e2a48496007e0140f6d60dc42
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2021-02-24 08:16:06 -0500

    Strip out "@" from the lambda package name when using namespaces

--- generators/aws/templates/infra/utils/lambda-config.ts
+++ generators/aws/templates/infra/utils/lambda-config.ts
@@ -4,7 +4,9 @@ import packageJson from '../../package.json';
 
 const { name, version } = packageJson;
 
-export const lambdaPackage = `dist/${name.replace(/\//g, '-')}-${version}.zip`;
+export const lambdaPackage = `dist/${name
+    .replace(/\//g, '-')
+    .replace(/@/g, '')}-${version}.zip`;
 export const lambdaDefaults = {
     runtime: Runtime.NODEJS_12_X,
     memorySize: 128,
